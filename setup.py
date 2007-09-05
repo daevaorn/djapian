54897216018fc390572a2735175fbdd3c19106af
Rafael Sierra <rafaeljsg14@gmail.com>
2007-09-05 15:25:44 +0000

    Bug fix: Issue #1

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
-setup( name="Djapian", version="1.0",
+setup( name="Djapian", version="1.0.1",
        author='Rafael "SDM" Sierra', author_email="rafaeljsg14@gmail.com",
        packages=[ "djapian", "djapian.backend" ], package_dir={ "djapian": "src" },
        scripts=['src/run_djapian.py']
