35b1abf254aaf087c520e8982496709fc88ee4e9
rafaeljsg14 <rafaeljsg14@36a948a3-fc37-0410-8bc3-7137da836232>
2007-09-12 13:28:39 +0000

    setup.py updated to the new source layout

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup( name="Djapian", version="1.0.1",
        author='Rafael "SDM" Sierra', author_email="rafaeljsg14@gmail.com",
-       packages=[ "djapian", "djapian.backend" ], package_dir={ "djapian": "src" },
-       scripts=['src/run_djapian.py']
-     )
+       packages=[ "djapian", "djapian.backend" ], package_dir={ "djapian": "src/djapian" },
+       scripts=['src/scripts/run_djapian.py']
+)
