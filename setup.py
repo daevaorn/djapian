0b7686ce16fbb0a4ef03991f3a72bec21e3d66ff
guillaume <guillaume@revolutionlinux.com>
2008-03-13 19:12:12 +0000

    the next release will be v 0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
-setup( name="Djapian", version="1.0.1",
+setup( name="Djapian", version="0.9",
        author='Rafael "SDM" Sierra', author_email="rafaeljsg14@gmail.com",
        packages=[ "djapian", "djapian.backend" ], package_dir={ "djapian": "src/djapian" },
        scripts=['src/scripts/run_djapian.py']
