a82dd733490f6cac74934d498820277fbc75ea03
daevaorn <daevaorn@36a948a3-fc37-0410-8bc3-7137da836232>
2008-09-21 17:17:20 +0000

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@
 from distutils.core import setup
 
 
-setup(name="Djapian", version="1.1",
+setup(name="Djapian", version="1.7",
       author='Rafael "SDM" Sierra',
       author_email="rafaeljsg14@gmail.com",
-      packages=["djapian", "djapian.backend", "djapian.management"],
+      packages=["djapian", "djapian.management"],
       package_dir={"djapian": "src/djapian"},
 )
