91d31e54214d79b6cd045019ef08b266f331f49b
daevaorn <daevaorn@36a948a3-fc37-0410-8bc3-7137da836232>
2009-02-14 23:59:54 +0000

    updated paginator test

--- src/djapian/tests/pagination.py
+++ src/djapian/tests/pagination.py
@@ -1,13 +1,29 @@
 from django.core.paginator import Paginator
 
-from djapian.tests.utils import BaseTestCase, BaseIndexerTest, Entry
+from djapian.tests.utils import BaseTestCase, Entry
 
-class PaginatorTest(BaseIndexerTest):
-    def test_paginator_creation(self):
-        paginator = Paginator(Entry.indexer.search("test"), 10)
+class ResultSetPaginationTest(BaseTestCase):
+    num_entries = 100
+    per_page = 10
+    num_pages = num_entries / per_page
 
-        self.assertEqual(paginator.num_pages, 1)
+    def setUp(self):
+        p = Person.objects.create(name="Alex")
 
-        page = paginator.page(1)
+        for i in range(self.num_entries):
+            Entry.objects.create(
+                author=p,
+                title="Entry with number %s" % i,
+                text="foobar " * i
+            )
 
-        self.assertEqual(len(page.object_list), 2)
+        Entry.indexer.update()
+
+        self.result = Entry.indexer.search("title:number")
+
+    def test_pagintion(self):
+        paginator = Paginator(self.result, self.per_page)
+
+        self.assertEqual(paginator.num_pages, self.num_pages)
+
+        page = paginator.page(5)
--- src/djapian/tests/search.py
+++ src/djapian/tests/search.py
@@ -18,35 +18,6 @@ class IndexerSearchTextTest(BaseIndexerTest, BaseTestCase):
     def test_score(self):
         self.assert_(self.result[0].percent in (99, 100))
 
-
-class ResultSetPaginationTest(BaseTestCase):
-    num_entries = 100
-    per_page = 10
-    num_pages = num_entries / per_page
-
-    def setUp(self):
-        p = Person.objects.create(name="Alex")
-
-        for i in range(self.num_entries):
-            Entry.objects.create(
-                author=p,
-                title="Entry with number %s" % i,
-                text="foobar " * i
-            )
-
-        Entry.indexer.update()
-
-        self.result = Entry.indexer.search("title:number")
-
-    def test_pagintion(self):
-        from django.core.paginator import Paginator
-
-        paginator = Paginator(self.result, self.per_page)
-
-        self.assertEqual(paginator.num_pages, self.num_pages)
-
-        page = paginator.page(5)
-
 class AliasesTest(BaseTestCase):
     num_entries = 10
 
