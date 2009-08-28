78beba4f689ecc9bfce5e4689b05f34ec66439fc
Alexander Koshelev <daevaorn@gmail.com>
2009-08-28 10:30:47 +0000

    Fixed index.py auto discovery

--- src/djapian/utils/loading.py
+++ src/djapian/utils/loading.py
@@ -3,6 +3,8 @@
 import os
 import imp
 
+from django.utils.importlib import import_module
+
 class NoModuleError(Exception):
     """
     Custom exception class indicates that given module does not exit at all
@@ -20,25 +22,4 @@ def get_module(base, module_name):
     except ImportError:
         raise NoModuleError("Cannot find module `%s` in base `%s`" % (module_name, base))
 
-    return getattr(__import__(base, {}, {}, [module_name]), module_name)
-
-def get_module_attrs(base, module_name, filter=lambda attr: True):
-    mod = get_module(base, module_name)
-
-    try:
-        attrs = mod.__all__
-    except AttributeError:
-        attrs = dir(mod)
-
-    return dict([(name, getattr(mod, name)) for name in attrs if filter(getattr(mod, name))])
-
-def list_sub_modules(base, module_name):
-    mod = get_module(base, module_name)
-
-    directory = os.path.dirname(os.path.abspath(mod.__file__))
-    names = [name for name in os.listdir(directory) if name.endswith('.py') and name != '__init__.py']
-    return [os.path.splitext(name)[0] for name in names]
-
-def get_sub_modules(base, module_name):
-    return [get_module("%s.%s" % (base, module_name), sub_mod_name)\
-                    for sub_mod_name in list_sub_modules(base, module_name)]
+    return import_module('.%s' % module_name, base)
