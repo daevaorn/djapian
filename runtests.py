c4bab5fa01f881c68b9598d12b3a6159225ed2cb
Evgeny Sizikov <esizikov@gmail.com>
2012-08-31 13:39:11 +0000

    Updated runtests.py to support Django 1.4

--- runtests.py
+++ runtests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import tempfile
+import shutil
 
 from django.conf import settings
 from django.core.management import call_command
@@ -8,8 +9,16 @@ from django.core.management import call_command
 settings.configure(
     INSTALLED_APPS=('django.contrib.contenttypes', 'djapian',),
     DATABASE_ENGINE='sqlite3',
+    DATABASE_NAME=':memory:',
+    DATABASES = {
+        'default': {
+            'NAME': ':memory:',
+            'ENGINE': 'django.db.backends.sqlite3',
+        },
+    },
     DJAPIAN_DATABASE_PATH=tempfile.mkdtemp(prefix='djapian'),
 )
 
 if __name__ == "__main__":
     call_command('test', '.'.join(['djapian'] + sys.argv[1:]))
+    shutil.rmtree(settings.DJAPIAN_DATABASE_PATH)
